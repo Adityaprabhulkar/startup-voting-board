1f288a209ec3e2070ce361fbf2f740242a19dd95
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-11-15 03:54:06 +0530

    Add more td

--- startup-voting-board/src/components/IdeaCard.jsx
+++ startup-voting-board/src/components/IdeaCard.jsx
@@ -3,14 +3,25 @@ import VoteButtons from "./VoteButtons";
 
 const IdeaCard = ({ idea }) => {
     return (
-        <div className="p-5 m-5 bg-[#eae3e3] border border-gray-300 overflow-hidden rounded-lg shadow">
+        <div className="p-5 m-5 bg-[#e8def7] border border-gray-300 overflow-hidden rounded-lg shadow">
             <img src={idea.avatar}
             alt= {idea.title}
-            className="w-[300px] rounded-full object-cover" />
+            className="w-[150px] rounded-full object-cover" />
 
-            <h3>{idea.title}</h3>
-            <p>{idea.description}</p>
-            <p>{idea.category}</p>
+            <h3 className="  mt-4 text-lg font-semibold 
+                border border-gray-300 bg-white 
+                px-3 py-1 rounded-md shadow-sm
+                w-full max-w-md">{idea.title}</h3>
+
+            <p  className="
+                text-sm text-gray-700 
+                border border-gray-300 bg-white 
+                px-3 py-1 rounded-md shadow-sm 
+                mt-0.5
+                w-full max-w-md
+                ">{idea.description}</p>
+
+
   
 
             <VoteButtons ideaId={idea.id}/>
