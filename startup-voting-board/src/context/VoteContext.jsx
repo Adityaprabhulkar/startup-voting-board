045e9c69cce24a1f65034ecd333b6ac95a446926
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-11-17 22:27:35 +0530

    Add local Stoarge

--- startup-voting-board/src/context/VoteContext.jsx
+++ startup-voting-board/src/context/VoteContext.jsx
@@ -3,37 +3,68 @@ import React, { createContext, useState, useEffect } from "react";
 export const VoteContext = createContext();
 
 export const VoteProvider = ({ children }) => {
-  
-  const storeVotes = JSON.parse(localStorage.getItem("votes")) || {};
-  const [votes, setVotes] = useState({});
+  const [ votes, setVotes ] = useState(() => {
+    return JSON.parse(localStorage.getItem("votes")) || {};
+  });
 
   useEffect(() => {
     fetch("https://6915dcd1465a9144626de9fb.mockapi.io/idea")
       .then((res) => res.json())
       .then((data) => {
-        const initialVotes = {};
+        const apiVotes = {};
+
+
         data.forEach((idea) => {
-          initialVotes[idea.id] = idea.votes || 0;
+          apiVotes[idea.id] = idea.votes || 0;
         });
-        setVotes(initialVotes);
+
+    setVotes((prev) => ({ ...apiVotes, ...prev }));
       });
-  }, []);
+    }, []);  
 
   useEffect(() => {
-    localStoarge.setItem("votes", JSON.stringify(votes));
+    localStorage.setItem("votes", JSON.stringify(votes));
   }, [votes]);
 
-  const upvote = (id) => {
-    setVotes((prev) => ({ ...prev, [id]: prev[id] + 1 }));
+   const upvote = (id) => {
+    setVotes((prev) => ({ ...prev, [id]: (prev[id] || 0) + 1 }));
   };
 
   const downvote = (id) => {
-    setVotes((prev) => ({ ...prev, [id]: prev[id] - 1 }));
+    setVotes((prev) => ({ ...prev, [id]: (prev[id] || 0) - 1 }));
   };
 
   return (
     <VoteContext.Provider value={{ votes, upvote, downvote }}>
-      {children}
+    {children}
     </VoteContext.Provider>
   );
 };
+//   const [votes, setVotes] = useState({});
+
+//   useEffect(() => {
+//     fetch("https://6915dcd1465a9144626de9fb.mockapi.io/idea")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         const initialVotes = {};
+//         data.forEach((idea) => {
+//           initialVotes[idea.id] = idea.votes || 0;
+//         });
+//         setVotes(initialVotes);
+//       });
+//   }, []);
+
+//   const upvote = (id) => {
+//     setVotes((prev) => ({ ...prev, [id]: (prev[id] || 0) + 1 }));
+//   };
+
+//   const downvote = (id) => {
+//     setVotes((prev) => ({ ...prev, [id]: (prev[id] || 0) - 1 }));
+//   };
+
+//   return (
+//     <VoteContext.Provider value={{ votes, upvote, downvote }}>
+//       {children}
+//     </VoteContext.Provider>
+//   );
+// };
